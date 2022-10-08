1b3643667f0c55bf420bd5f7632d884fca426fc8
Huang-siyuan <975892929@qq.com>
2022-10-08 17:35:01 +0300

    Init submit.

--- src/main/java/com/itmo/soalab2/SoaLab2Application.java
+++ src/main/java/com/itmo/soalab2/SoaLab2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoaLab2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoaLab2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoaLab2Application.class, args);
+    }
 
 }
